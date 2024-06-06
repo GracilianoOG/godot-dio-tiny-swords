f3501eaf2d82f1a8f11138305d1f36701578dd3d
Gabriel <gabriel.gracilianomb@gmail.com>
2024-06-06 11:26:50 -0300

    Change meat words to skull

--- ui/game_ui.gd
+++ ui/game_ui.gd
@@ -2,10 +2,10 @@ extends CanvasLayer
 
 @onready var timer_label: Label = %TimerLabel
 # @onready var gold_label: Label = %GoldLabel
-@onready var meat_label: Label = %MeatLabel
+@onready var skull_label: Label = %SkullLabel
 
 
 func _process(delta: float):
 	# Update labels
 	timer_label.text = GameManager.time_elapsed_string
-	meat_label.text = str(GameManager.monsters_defeated_counter)
+	skull_label.text = str(GameManager.monsters_defeated_counter)
