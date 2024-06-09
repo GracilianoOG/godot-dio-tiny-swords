d7edd63bb91fd0880fac02e31f9b21139532221a
Gabriel <gabriel.gracilianomb@gmail.com>
2024-06-09 17:46:26 -0300

    Remove auto restart from game over

--- ui/game_over_ui.gd
+++ ui/game_over_ui.gd
@@ -1,10 +1,6 @@
 class_name GameOverUI
 extends CanvasLayer
 
-@export var restart_delay: float = 5.0
-
-var restart_cooldown: float
-
 @onready var time_label: Label = %TimeLabel
 @onready var monsters_label: Label = %MonstersLabel
 
@@ -12,18 +8,6 @@ var restart_cooldown: float
 func _ready():
 	time_label.text = GameManager.time_elapsed_string
 	monsters_label.text = str(GameManager.monsters_defeated_counter)
-	restart_cooldown = restart_delay
-
-
-func _process(delta):
-	restart_cooldown -= delta
-	if restart_cooldown <= 0.0:
-		restart_game()
-
-
-func restart_game():
-	GameManager.reset()
-	get_tree().reload_current_scene()
 
 
 func _on_menu_pressed():
@@ -31,4 +15,5 @@ func _on_menu_pressed():
 
 
 func _on_restart_pressed():
-	restart_game()
+	GameManager.reset()
+	get_tree().reload_current_scene()
