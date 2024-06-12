52c63e4fe364961d534e12d9d3a485d1769e9d34
Gabriel <gabriel.gracilianomb@gmail.com>
2024-06-12 16:04:24 -0300

    Add script to go to the credits scene

--- ui/game_menu.gd
+++ ui/game_menu.gd
@@ -8,3 +8,7 @@ func _on_exit_btn_pressed():
 func _on_start_btn_pressed():
 	GameManager.reset()
 	get_tree().change_scene_to_file("res://main.tscn")
+
+
+func _on_credits_btn_pressed():
+	get_tree().change_scene_to_file("res://ui/game_credits.tscn")
